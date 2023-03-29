6a3950abea5c34a2f59a784a257e60b574beece3
Cheolhyun Mun <zooxop@gmail.com>
2023-03-29 20:26:06 +0900

    feat: initial project

---
+++ EasyBorderFrame/EasyBorderFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBorderFrameApp.swift
+//  EasyBorderFrame
+//
+//  Created by 문철현 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct EasyBorderFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
